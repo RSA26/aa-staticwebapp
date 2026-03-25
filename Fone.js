7d385befa88b2d296580968e3cb4f5788665a5c1
vkitx <vkitxx@gmail.com>
2026-03-25 16:20:31 +0100

    oppdater js

--- Fone.js
+++ Fone.js
@@ -1,36 +1,31 @@
-// 1. Data om sjåførene (Objektet ditt, ferdig fikset)
+//data om sjåførene
 const drivers = {
-    "Max Verstappen": {
-        team: "Red Bull Racing",
-        wins: 61,
-        color: "#0600EF", // Red Bull Blå
-        image: "https://img.redbull.com"
-    },
-    "Lewis Hamilton": {
-        team: "Scuderia Ferrari",
-        wins: 105,
-        color: "#E10600", // Ferrari Rød
-        image: "https://img2.51gt3.com"
-    },
-    "Charles Leclerc": {
-        team: "Scuderia Ferrari",
-        wins: 8,
-        color: "#E10600",
-        image: "https://img2.51gt3.com"
+    "max Verstappen":{
+team: "Red Bul"
+wins : 60
+color: "red",
+image:
     }
+
+    "Lewis Hamilton":{
+        team:"Scuderia Ferrari"
+        wins: 103
+        color: "red"
+        image
+
+    }
+
+    "Charles Leclerc"{
+        team: "ferrari"
+
+    }
+
 };
 
-// 2. Funksjon for å oppdatere siden når du velger en sjåfør
-function visSjafor(navn) {
-    const sjafor = drivers[navn];
-    
-    // Oppdaterer overskriften (h2 id="info")
-    document.getElementById("info").innerText = navn + " - " + sjafor.team;
-    
-    // Oppdaterer bildet (img id="driverimg")
-    const bildeElement = document.getElementById("driverimg");
-    if (bildeElement) {
-        bildeElement.src = sjafor.image;
-        bildeElement.style.borderColor = sjafor.color;
-    }
+{
+    const info=document.getElementById("info");
+    const img=document.getElementById("driverimg");
+info.innerText='${name}-${drivers[name].team}-Wins: ${drivers[name].wins}';
+info.style.color =drivers[name].color;
+img.src =drivers[name].image;
 }
