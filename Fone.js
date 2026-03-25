ba515f3324cb56773fe32215717ea033f9fa85e0
vkitx <vkitxx@gmail.com>
2026-03-25 15:10:42 +0100

    javascript

---
+++ Fone.js
@@ -0,0 +1,23 @@
+//data om sjåførene
+const drivers = {
+    "max Verstappen":{
+team: "Red Bul"
+wins : 60
+color: "red",
+image:
+    }
+
+    "Lewis Hamilton":{
+        team:"Scuderia Ferrari"
+        wins: 103
+        color: "red"
+        image
+
+    }
+
+    "Charles Leclerc"{
+        team: "Ferrari"
+        
+    }
+
+}
